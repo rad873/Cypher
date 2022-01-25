218b858c526d8b2b5e12ad6c55c8b7ef997d469f
kevindnwu <97580805+kevindnwu@users.noreply.github.com>
2022-01-25 12:51:58 -0600

    Update Cypher.py

--- Cypher.py
+++ Cypher.py
@@ -1,1 +1,8 @@
-
+def histogram(string):
+    word_dict = dict()
+    for letter in string:
+        if letter not in word_dict:
+            word_dict[letter]= 1
+        else:
+            word_dict[letter] += 1
+    return word_dict

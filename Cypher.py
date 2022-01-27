ed3688a30821dde4df8d62e6fc25010df3dbb3da
kevindnwu <97580805+kevindnwu@users.noreply.github.com>
2022-01-27 13:24:08 -0600

    Update Cypher.py

--- Cypher.py
+++ Cypher.py
@@ -1,10 +1,13 @@
+def decrypt(file,x):
+    partial_keys = get_all_partial_keys(['e', 't', 'a', 'o'], LetterCounter(file, 4)) #use partial keys to generate dictionaries set
+    key = partial_keys[x]  #x is the set of keys out of all the possible dictonary, use as testing
+    print(key)
+    with open(file) as word_list:  
+        words_list = word_list.read().split()
+    print(words_list)
 
-def histogram(string):
-    word_dict = dict()
-    for letter in string:
-        if letter not in word_dict:
-            word_dict[letter]= 1
-        else:
-            word_dict[letter] += 1
-    return word_dict
-histogram('banana')
+    open('cypher_output.txt', 'w').close()
+    #for word in words_list:
+        #for letter in word:
+           # if letter == key[key]:
+                

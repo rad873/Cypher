4a057c60f0fec8aef085172d7b21af5c7846faad
Max Rademacher <82227369+rad873@users.noreply.github.com>
2022-01-27 16:10:00 -0600

    Update Cypher.py

--- Cypher.py
+++ Cypher.py
@@ -10,4 +10,24 @@ def decrypt(file,x):
     #for word in words_list:
         #for letter in word:
            # if letter == key[key]:
+            
                 
+def decrypt(file, x): #this prints the set of all partial decryptions
+    partial_keys = get_all_dictionaries(['e','i','a'],LetterCounter(file,x))  # use partial keys to generate dictionaries set
+    key = partial_keys[x]  # x is the set of keys out of all the possible dictonary, use as testing
+    letters = []
+    partial = []
+    with open(file) as words_list:
+        words_list = words_list.read().split()
+        for key in partial_keys:#{}
+            for item in key: #eta
+                for word in words_list: #VQVXGVW
+                    for letter in word: #V
+                        if key[item] == letter:
+                            letter = item
+                        letters.append(letter)
+                    partial.append(letters)
+                    letters = []
+            print(partial)
+
+    open(file).close()
